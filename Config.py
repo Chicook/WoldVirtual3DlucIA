466520a3d08a583921ec2f663d9aa91c235cca86
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2025-07-15 20:44:59 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@
 ## por lo que se recomienda hacer refactorizacion y pruebas en .. #
 ## entorno seguro como un directorio de pruebas ##
 ## cullos archivos sea fácilmente recuperables. ##
-"""
+
 
 WoldVirtual3DlucIA - Sistema de Seguridad Global v0.6.0
 Config.py - Gestor de Dependencias y Protección de APIs
